2e5f79b54920c140a9edab22d2db1a90b864fde4
ZHAI-lixiaoyue <87556775+ZHAI-lixiaoyue@users.noreply.github.com>
2021-08-17 11:37:08 +0800

    ......

--- srkl/js/index.js
+++ srkl/js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||ZH|AI|祝|你|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||ZH|AI|祝你|生日|快乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
